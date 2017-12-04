c574b62824c8f11ef1874eab3cddef05d1973b02
Neil Singh <Neil@Neils-MacBook-Pro.local>
2017-12-04 13:43:18 -0600

    Add some debug info
    Change-Id: I9e46005a09d1ab69ce890b11f0e6a7a990d6fb7e

--- src/mem/dram_ctrl.cc
+++ src/mem/dram_ctrl.cc
@@ -227,6 +227,7 @@ DRAMCtrl::init()
     }
 
     FILE* file = fopen(PROFILE_NAME, "r");
+    printf("Reading profile data from %s\n", PROFILE_NAME);
     if (!file)
         panic("Could not read profile file\n");
     // TODO: read file data and send it to sepcialized cache
